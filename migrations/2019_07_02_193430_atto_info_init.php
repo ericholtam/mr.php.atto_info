b979e4f01161fd8bf3deac9559faec56d60d28aa
admin <admin@pmunki1dsm.ad.mdp.com>
2019-07-02 15:14:22 -0500

    Migration fixed to allow non-unique SN

--- migrations/2019_07_02_193430_atto_info_init.php
+++ migrations/2019_07_02_193430_atto_info_init.php
@@ -10,7 +10,7 @@ class AttoInfoInit extends Migration
         $capsule = new Capsule();
         $capsule::schema()->create('atto_info', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('serial_number')->unique();
+            $table->string('serial_number');
             $table->string('channel')->nullable();
             $table->string('model')->nullable();
             $table->string('port_state')->nullable();
