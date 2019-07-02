e48c9d7282bb196ec85ef353748de0ad1ad02710
admin <admin@pmunki1dsm.ad.mdp.com>
2019-07-02 15:32:49 -0500

    Tab fixup

--- views/atto_info_tab.php
+++ views/atto_info_tab.php
@@ -5,30 +5,35 @@
 
 <script>
 $(document).on('appReady', function(){
-	$.getJSON(appUrl + '/module/atto_info/get_data/' + serialNumber, function(data){
-		// Set count of atto_info
-		$('#atto_info-cnt').text(data.length);
-		var skipThese = ['id', 'serial_number'];
-		$.each(data, function(i,d){
+        $.getJSON(appUrl + '/module/atto_info/get_data/' + serialNumber, function(data){
+                // Set count of atto_info
+                $('#atto_info-cnt').text(data.length);
+ 		        var skipThese = ['id', 'serial_number', 'channel'];
+                $.each(data, function(i,d){
 
-			// Generate rows from data
-			var rows = ''
-			for (var prop in d){
-				rows = rows + '<tr><th>'+i18n.t('atto_info.'+prop)+'</th><td>'+d[prop]+'</td></tr>';
-				}
-			}
-			$('#atto_info-tab')
-				.append($('<h4>')
-					.append($('<i>')
-						.addClass('fa fa-hdd-o'))
-					.append(' '+d.name))
-				.append($('<div>')
-					.addClass('table-responsive')
-					.append($('<table>')
-						.addClass('table table-striped table-condensed')
-						.append($('<tbody>')
-							.append(rows))))
-		})
-	});
+                    // Generate rows from data
+                    var rows = ''
+                    for (var prop in d){
+                    // Skip skipThese
+                    if(skipThese.indexOf(prop) == -1){
+                        if(prop != 'id' && prop != 'serial_number' && prop != 'channel'){
+                            rows = rows + '<tr><th>'+i18n.t('atto_info.'+prop)+'</th><td>'+d[prop]+'</td></tr>';
+                            }
+                        }
+                    }    
+                    $('#atto_info-tab')
+                            .append($('<h4>')
+                                    .append($('<i>')
+                                            .addClass('fa fa-hdd-o'))
+                                    .append(' '+d.channel))
+                            .append($('<div>')
+                                    .addClass('table-responsive')
+                                    .append($('<table>')
+                                            .addClass('table table-striped table-condensed')
+                                            .append($('<tbody>')
+                                                    .append(rows))))
+                })
+        });
 });
 </script>
+
